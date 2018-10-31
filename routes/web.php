17ad7e06423d5f81de79e86ec5a8dac8eb47a1ae
George Jacob <georgekunchattil@gmail.com>
2018-10-31 22:48:17 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| It contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more
+|
+*/
+
+Route::group(['middleware' => ['web']], function(){
+
+});
